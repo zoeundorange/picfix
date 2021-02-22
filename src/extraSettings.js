ecb1391191495a8285f0f44b3cb6629fd1475c34
zoeundorange <43229317+zoeundorange@users.noreply.github.com>
2021-02-23 06:00:17 +0900

    Update extraSettings.js

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -9,7 +9,7 @@ let extraScore = 1;
 let wuDi = true;
 
 // 第一个水果：修改数字为 0-10, 0 为葡萄，9 为半个西瓜
-let firstFruit = 0;
+let firstFruit = 9;
 
 // 水果合成反转：false 改为 true
 let reverseLevelUp = true;
@@ -26,7 +26,7 @@ let setFruits = {
 }
 
 // 让水果更 Q 弹：false 改为大于 0 小于 1 的任意小数（推荐 0.9）
-let fruitQTan = 0.6;
+let fruitQTan = 0.9;
 
 // 让水果下落缓慢：false 改为大于 0 的任意数，值越大阻力越大，下落越慢（推荐 5）
 let fruitSlowDown = false;
